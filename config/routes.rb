a2c676eff7e73ecc647ed61d92d5d7f062a4537f
Shomukhammad <shomuhammad404@gmail.com>
2022-12-12 13:29:42 +0300

    update

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :projects
+  devise_for :users
+  root 'pages#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
